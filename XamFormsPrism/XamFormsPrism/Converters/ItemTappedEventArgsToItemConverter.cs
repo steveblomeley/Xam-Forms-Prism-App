ca9349de0290ef44a818c40710e975cdde6389a2
steve <>
2018-07-13 10:30:00 +0100

    Xaml changes - add a listview to the main page, with reference to prism's event-to-command mapper, and local converter to capture & convert the event args

---
+++ XamFormsPrism/XamFormsPrism/Converters/ItemTappedEventArgsToItemConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+// ReSharper disable once CheckNamespace
+namespace XamFormsPrism
+{
+    public class ItemTappedEventArgsToItemConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value is ItemTappedEventArgs eventArgs
+                ? eventArgs.Item
+                : throw BadValueTypeException();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+        private Exception BadValueTypeException()
+        {
+            return new ArgumentException(
+                $"{nameof(ItemTappedEventArgsToItemConverter)}.{nameof(Convert)} - expected \"value\" argument to be of type {nameof(SelectedItemChangedEventArgs)}.");
+        }
+    }
+}
